637c01d16786f7d736cc21765adcbe8765b060f8
Paramita S <judge.27miyoo@gmail.com>
2023-05-22 01:29:14 +0700

    edit todo and readme

--- Backend/index.js
+++ Backend/index.js
@@ -49,41 +49,6 @@ app.patch("/editTodo", require("./route/EditTodo"));
 //unfinish
 app.get("/geTodoStatus", require("./route/GetStatus"));
 
-// app.post("/todoadd", (req, res) => {
-//     // The JSON body
-//     const { userId } = res.locals;
-//     const { name, deadline, time, description } = req.body;
-//     const status = "not complete";
-
-//     connection.query(
-//         `INSERT INTO todos (user_id, name, deadline, time, description, status) 
-//          VALUES (?, ?, ?, ?, ?, ?)`, 
-//         [userId, name, deadline, time, description, status], 
-//         (err, rows) => {
-//             // Check if cannot find the data in the database then return the error
-//             if (err) {
-//                 res.json({
-//                     success: false,
-//                     data: null,
-//                     error: err.message,
-//                 });
-//             } else {
-//                 // Return data to the client if success
-//                 console.log(rows);
-//                 if (rows) {
-//                     res.json({
-//                         success: true,
-//                         data: {
-//                             message: "create success",
-//                         },
-//                     });
-//                 }
-//             }
-//         }
-//     );
-// });
-
-
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });

7d1ef91dc4f1b4aeebe8093e6f5da192e0cb0ab9
Paramita Saenghao <paramita.saen@mail.kmutt.ac.th>
2023-06-02 11:38:29 +0700

    edit database

--- Backend/route/Register.js
+++ Backend/route/Register.js
@@ -5,9 +5,7 @@ module.exports = async (req, res) => {
 	const { username, email, password } = req.body;
 	const profile = "https://t4.ftcdn.net/jpg/04/10/43/77/360_F_410437733_hdq4Q3QOH9uwh0mcqAhRFzOKfrCR24Ta.jpg";
 	const salt1 = await bcrypt.genSalt(10);
-	// console.log("Salt #1: ", salt1);
 	const hash1 = await bcrypt.hash(password, salt1);
-	// console.log("Hash #1: ", hash1);
 
 	// Check if username already exists
 	var checkUsernameQuery = mysql.format("SELECT * FROM users WHERE username = ? OR email = ?", [username, email]);
@@ -31,8 +29,8 @@ module.exports = async (req, res) => {
 
 		// Username does not exist, proceed with registration
 		var insertQuery = mysql.format(
-			"INSERT INTO users (username, email, password, hashed_password, profile_picture) VALUES (?, ?, ?, ?, ?)",
-			[username, email, password, hash1, profile]
+			"INSERT INTO users (username, email, hashed_password, profile_picture) VALUES (?, ?, ?, ?)",
+			[username, email, hash1, profile]
 		);
 
 		connection.query(insertQuery, (err, rows) => {
